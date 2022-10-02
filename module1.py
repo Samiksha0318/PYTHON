7e6b466a7c51e3df070564535bbca0fd0f4facae
Samiksha <114793981+Samiksha0318@users.noreply.github.com>
2022-10-02 13:04:03 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,97 @@
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+
+name = "Sam"
+print(name)
+
+#Arithmetic Operators
+
+#Addition
+print(3+2)
+
+#Subtraction
+print(3-2)
+
+#Division
+print(3 / 2)
+
+#Floor Division
+print(3 // 2)
+
+#Multiplication
+print(3 * 2)
+
+#Exponent
+print(3 ** 2)
+
+#Modulus
+print(3 % 2)
+
+# for a % b => a = q * b + remainder
+print(-4 % 3)
+
+#-2 * 3 + 2
+
+#Complex Assi.
+num = 4
+num = num + 5
+print(num)
+
+num = 4
+num += 5
+print(num)
+
+num=4
+num *=5
+print(num)
+
+#Absolute Function : we will only get magnitude
+#abs(number)
+print(abs(-4))
+
+#Round Function
+#round(number,digits)
+print(round(2.59))
+
+print(round(2.7482 , 2))
+
+#Comparisons
+
+#Equal : ==
+#Not Equal : !=
+#Less Than : <
+#Greater Than : >
+#Less or Equal : <=
+#Greater or Equal : >=
+
+num1 = 4
+num2 = 3
+print(num1 > num2)
+
+s = 10.6
+print(type(s))
+
+message = "Welcome to Sam's World"
+print(message)
+
+message = 'Welcome to "Sam" World'
+print(message)
+
+message = """Hello Coders
+Welcome to my World"""
+print(message)
+
+#This is a method
+message = "hello world"
+print(message.upper())
+
+message = "HELLO WORLD"
+print(message.lower())
+
+message = "hello"
+message = message.upper()
+print(message)
+
+
